a0ae0c2f18826cdda514f28fc20c2d6b5822eda6
Rubén <ruben.sr.apple@gmail.com>
2024-05-07 13:01:54 +0200

    Create Xcode Project

---
+++ videos-swift-data/videos-swift-data/videos_swift_dataApp.swift
@@ -0,0 +1,17 @@
+//
+//  videos_swift_dataApp.swift
+//  videos-swift-data
+//
+//  Created by Ruben on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct videos_swift_dataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
